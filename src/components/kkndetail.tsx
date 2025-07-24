8e6d483757fa8298dd3c9a6ed895efbe573d7a53
Afito Indra Permana <indraafito56@gmail.com>
2025-07-24 18:48:17 +0700

    Ubah ukuran foto pada kkndetail

--- src/components/kkndetail.tsx
+++ src/components/kkndetail.tsx
@@ -72,14 +72,14 @@ const KknDetail = () => {
 
       {/* Main */}
       <div className="max-w-4xl mx-auto p-4 pb-8">
-        <div className="bg-white rounded-2xl overflow-hidden">
+        <div className="bg-white overflow-hidden">
           {/* Foto */}
           {selectedProgram.photos && selectedProgram.photos.length > 0 && (
-            <div className="relative h-64 md:h-80">
+            <div className="relative h-1/2 md:h-128">
               <img
                 src={selectedProgram.photos[currentIndex]}
                 alt={`${selectedProgram.nama_proker} - Foto ${currentIndex + 1}`}
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover rounded-2xl"
                 onError={(e) => {
                   e.currentTarget.src = "/placeholder-image.jpg";
                 }}
